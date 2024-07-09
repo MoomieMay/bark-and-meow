06cdfc5afaf85b205db622760add7b318d196b2b
MayDay <61630498+MoomieMay@users.noreply.github.com>
2024-07-08 21:43:48 -0300

    Initial Commit

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaShoppingCart } from 'react-icons/fa';
+
+const CartWidget = () => {
+  return (
+    <div className="cart-widget">
+      <FaShoppingCart />
+      <span className="cart-count">0</span>
+    </div>
+  )
+}
+
+export default CartWidget
